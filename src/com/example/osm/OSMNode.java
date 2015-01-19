a89ffeadeddcc7e5c1d33be353d791d36ac28eda
Harun Sentürk <harun.sentuerk@gmx.de>
2015-01-19 22:30:37 +0100

    initial commit

---
+++ src/com/example/osm/OSMNode.java
@@ -0,0 +1,17 @@
+package com.example.osm;
+
+import java.util.Map;
+
+public class OSMNode {
+	
+	private String id;
+	
+	private String lat;
+	
+	private String lon;
+	
+	private final Map<String, String> tags;
+
+	private String version;
+
+}
