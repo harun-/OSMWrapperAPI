4ddc8d5b9fd20729820c073ca095941b00e98329
Harun Sentürk <harun.sentuerk@gmx.de>
2015-01-20 23:33:47 +0100

    implemented BasicAuthenticator

--- src/com/example/osm/BasicAuthenticator.java
+++ src/com/example/osm/BasicAuthenticator.java
@@ -1,7 +1,23 @@
 package com.example.osm;
 
 import java.net.Authenticator;
+import java.net.PasswordAuthentication;
 
 public class BasicAuthenticator extends Authenticator {
+	
+	private final PasswordAuthentication passwordAuth;
+	
+	public BasicAuthenticator(String userName, String password) {
+		this.passwordAuth = new PasswordAuthentication(userName, password.toCharArray());
+	}
+	
+	public BasicAuthenticator(PasswordAuthentication passwordAuth) {
+		this.passwordAuth = passwordAuth;
+	}
+
+	@Override
+	protected PasswordAuthentication getPasswordAuthentication() {
+		return passwordAuth;
+	}
 	
 }
